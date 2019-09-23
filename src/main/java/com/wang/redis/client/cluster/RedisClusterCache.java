a36fc43e6bff25cadfcf57e574e0afeaf31be7a0
wjx <731461008@qq.com>
2019-09-23 10:29:15 +0800

    solt

--- src/main/java/com/wang/redis/client/cluster/RedisClusterCache.java
+++ src/main/java/com/wang/redis/client/cluster/RedisClusterCache.java
@@ -1,6 +1,9 @@
 package com.wang.redis.client.cluster;
 
+import com.wang.redis.Command.Command;
+import com.wang.redis.connection.ConnectionPool;
 import com.wang.redis.connection.impl.ClusterPoolImpl;
+import com.wang.redis.result.ObjectResult;
 import com.wang.redis.transmission.HostInfo;
 
 import java.util.ArrayList;
@@ -9,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 public class RedisClusterCache {
+    public static final String CLUSTER_SLOTS = "slots";
 
     private final Map<String, ClusterPoolImpl> nodes = new HashMap<>();
     private final Map<Integer, ClusterPoolImpl> slots = new HashMap<>();
@@ -79,14 +83,26 @@ public class RedisClusterCache {
             return hostInfo;
         } else {
             //初始化从新，根据node的缓存map
-//            connectionPool = cache.getSlotPool(slot);
-//            if (connectionPool != null) {
-//                return connectionPool.getResource();
-//            } else {
-//                //no choice, fallback to new connection to random node
-//                return getConnection();
-//            }
-            return null;
+            List<ClusterPoolImpl> pools = new ArrayList<>(nodes.values());
+            for(ClusterPoolImpl pool : pools){
+                createSlots(pool);
+            }
+            connectionPool = slots.get(slot);
+            HostInfo hostInfo = new HostInfo(connectionPool.getAddress(),connectionPool.getPort());
+            return hostInfo;
+        }
+    }
+
+    private void createSlots(ConnectionPool pool){
+        RedisClusterClient redisClusterClient = new RedisClusterClient(pool);
+        List<Object> slots = redisClusterClient.doExecute(Command.cluster, ObjectResult.class,CLUSTER_SLOTS);
+        for(Object slot : slots){
+            List<Object> slotInfo = (List<Object>) slot;
+            if (slotInfo.size() <= 2) {
+                continue;
+            }
+            //根据配置的集群host,发现其他的节点
+            this.getAllNodeInfo(slotInfo);
         }
     }
 
